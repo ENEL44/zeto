e893be444f8aebabf68d04ac2b277c670cad522c
ENEL44 <vikingrp416@gmail.com>
2024-07-03 11:30:20 +0530

    Add files via upload
    gen2 pokemon tile

---
+++ abomasnow/evos_attacks.asm
@@ -0,0 +1,21 @@
+	db 0 ; no more evolutions
+	db 1, TACKLE
+	db 1, VINE_WHIP
+	db 4, POWDER_SNOW
+	db 7, LEER
+	db 10, POWDER_SNOW
+	db 13, LEECH_SEED
+	db 16, VINE_WHIP
+	db 19, ICY_WIND
+	db 22, RAZOR_LEAF
+	db 25, AURORA_BEAM
+	db 28, ENERGY_BALL
+	db 31, ICE_PUNCH
+	db 34, SEED_BOMB
+	db 37, ICE_BEAM
+	db 40, LEECH_SEED
+	db 43, ENERGY_BALL
+	db 46, ICE_PUNCH
+	db 49, SEED_BOMB
+	db 52, BLIZZARD
+	db 0 ; no more level-up moves
